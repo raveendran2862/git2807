bdab8eac2543ebb2307d2fc902e0a1b510001bd7
Ravi <Ravi123@gmail.com>
2025-07-29 10:43:58 +0530

    added multiplication

--- calc.py
+++ calc.py
@@ -3,5 +3,12 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+print("Multiplication of 5 and 3:", multiply(5, 3))
+
 print("Addition of 5 and 3:", add(5, 3))
 print("Subtraction of 5 and 3:", subtract(5, 3))
